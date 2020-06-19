e057f0174a859a33e5924190f647e0f449c07211
Sven Linstaedt <sven.linstaedt@gmail.com>
2020-06-19 09:53:48 +0200

    feat: allow two step release

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,13 +15,24 @@ _params="$_params $(env2args camel '--$k "$v"' PLUGIN_SR_)"
 
 _msg=${DRONE_COMMIT_MESSAGE:-''}
 _prefix=${PLUGIN_SKIP_ON_COMMIT_PREFIX:-'chore(release)'}
-if test "${_msg:0:${#_prefix}}" != "$_prefix"; then
-	echo "standard-version $@ $_params"
-	standard-version $@ $_params
-	if test -n "${DRONE_WORKSPACE:-}" && test "${PLUGIN_SKIP_PUSH:-}" != "true"; then
-		git push
-		git push --follow-tags
-	fi
+if test "${_msg:0:${#_prefix}}" != "$_prefix" && test -n "${DRONE_WORKSPACE:-}"; then
+	case ${PLUGIN_MODE:-push} in
+		push)
+			echo "standard-version $@ $_params"
+			standard-version $@ $_params
+			git push --follow-tags
+			;;
+		prepare)
+			echo "standard-version $@ $_params"
+			standard-version $@ $_params
+			git checkout -d $(git describe --exact-match --abbrev=0)
+			;;
+		perform)
+			_branch=$(git for-each-ref --format='%(objectname) %(refname:short)' refs/heads | awk "/^$(git rev-parse HEAD)/ {print \$2}")
+			git checkout $_branch
+			git push --follow-tags
+			;;
+	esac
 else
 	# Bitbucket ignores [SKIP CI]
 	echo "Skipping release for commit: $_msg"
